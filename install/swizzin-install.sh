051e8d99f69b10c721a5f218733fd174bbb988f1
DESKTOP-UULMCR6\Joshua <JoshuaShabanov@gmail.com>
2025-08-01 18:47:33 +0300

    no message

--- install/swizzin-install.sh
+++ install/swizzin-install.sh
@@ -16,7 +16,7 @@ update_os
 msg_info "Installing Dependencies"
 $STD apt-get install -y curl wget gnupg lsb-release
 
-msg_info "Launching upstream Swizzin installer..."
+echo -e "Launching upstream Swizzin installer..."
 bash <(curl -sL git.io/swizzin)
 msg_info "Cleaning up"
 $STD apt-get -y autoremove
