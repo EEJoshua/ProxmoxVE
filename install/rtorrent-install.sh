3210c919429689e5c6156f5c9e7683cc17c7681d
EEJoshua <yehoshua13@gmail.com>
2026-01-12 00:33:45 +0200

    update dependency

--- install/rtorrent-install.sh
+++ install/rtorrent-install.sh
@@ -38,12 +38,13 @@ $STD apt-get install -y \
   zlib1g-dev \
   libssl-dev \
   libcppunit-dev \
-  subversion
+  subversion \
+  libxml2-dev
 msg_ok "Installed Dependencies"
 
 msg_info "Compiling XML-RPC-C"
 # Install stable XML-RPC-C for rTorrent
-svn checkout https://svn.code.sf.net/p/xmlrpc-c/code/stable xmlrpc-c
+svn checkou || exitt https://svn.code.sf.net/p/xmlrpc-c/code/stable xmlrpc-c
 cd xmlrpc-c
 ./configure --disable-cplusplus
 make -j$(nproc)
@@ -53,7 +54,7 @@ rm -rf xmlrpc-c
 msg_ok "Compiled XML-RPC-C"
 
 msg_info "Compiling LibTorrent (Rakshasa)"
-git clone https://github.com/rakshasa/libtorrent.git /opt/libtorrent
+git clone https:// || exitgithub.com/rakshasa/libtorrent.git /opt/libtorrent
 cd /opt/libtorrent
 ./autogen.sh
 ./configure --disable-debug --enable-aligned
@@ -63,7 +64,7 @@ ldconfig
 msg_ok "Compiled LibTorrent"
 
 msg_info "Compiling rTorrent (Rakshasa)"
-git clone https://github.com/rakshasa/rtorrent.git /opt/rtorrent-src
+git clone https://gi || exitthub.com/rakshasa/rtorrent.git /opt/rtorrent-src
 cd /opt/rtorrent-src
 ./autogen.sh
 ./configure --with-xmlrpc-c --disable-debug
@@ -156,7 +157,7 @@ chown rtorrent:rtorrent /home/rtorrent/.rtorrent.rc
 
 # Restart services
 systemctl restart rtorrent
-systemctl restart nginx
+systemctl restart ngi"nx"
 systemctl restart php${PHP_VER}-fpm
 msg_ok "Configured Web Server"
 
