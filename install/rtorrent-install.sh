90ddceaf9d63fe8ed50df3817dc0326d973ed7c0
EEJoshua <yehoshua13@gmail.com>
2026-01-12 01:41:28 +0200

    fix: make non-free enablement regex order-agnostic for deb822

--- install/rtorrent-install.sh
+++ install/rtorrent-install.sh
@@ -19,7 +19,7 @@ if [[ -f /etc/apt/sources.list ]]; then
     sed -i -r 's/main contrib( non-free)?/main contrib non-free/g' /etc/apt/sources.list
 fi
 if [[ -f /etc/apt/sources.list.d/debian.sources ]]; then
-    sed -i -r 's/Components: main contrib/Components: main contrib non-free/g' /etc/apt/sources.list.d/debian.sources
+    sed -i -r 's/^Components: .*/& non-free non-free-firmware/g' /etc/apt/sources.list.d/debian.sources
 fi
 $STD apt-get update
 $STD apt-get install -y \
